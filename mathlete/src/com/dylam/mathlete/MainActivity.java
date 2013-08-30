2d007a6a0e4583edb521d92954a2e58cb9c604c3
David Lam <dylam07@gmail.com>
2013-08-30 12:26:04 -0700

    Generates two numbers. User can input answer. Need to put in timer and get keyboard to stay visible.

--- mathlete/src/com/dylam/mathlete/MainActivity.java
+++ mathlete/src/com/dylam/mathlete/MainActivity.java
@@ -1,15 +1,62 @@
 package com.dylam.mathlete;
 
-import android.os.Bundle;
+import java.util.Random;
+
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnKeyListener;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-
+	int min = 0;
+	int max = 100;
+	int correct_count = 0;
+	String TAG = "MentalMath";
+	int a;
+	int b;
+	TextView number1;
+	TextView number2;
+	Random r;
+	EditText user_input;
+	ProgressBar mProgressBar; 
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		// Generate numbers
+		r = new Random();
+		number1 = (TextView)findViewById(R.id.number_1);
+		number2 = (TextView)findViewById(R.id.number_2);
+		
+		// Set up key listeners.
+		user_input = (EditText)findViewById(R.id.user_answer);
+		user_input.setOnKeyListener(new OnKeyListener() {
+
+			@Override
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				if (event.getAction() == KeyEvent.ACTION_DOWN) {
+					switch(keyCode) {
+					case KeyEvent.KEYCODE_ENTER:	
+						on_submit(null);
+						return true;
+					}
+					
+				}
+				
+				return false;
+			}} );
+		generateNumbers();
+		
+		mProgressBar = (ProgressBar)findViewById(R.id.progressBar1);
 	}
 
 	@Override
@@ -18,5 +65,34 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	private void generateNumbers() {
+		a = r.nextInt(this.max - this.min);
+		b = r.nextInt(this.max - this.min);
+		
+		number1.setText(Integer.toString(a));
+		number2.setText(Integer.toString(b));
+	}
+	
+	public void on_submit(View v) {
+		// Get input from user
+		int answer = Integer.parseInt(((EditText) findViewById(R.id.user_answer)).getText().toString());
+		Log.d(TAG, "User entered: " + Integer.toString(answer));
+		
+		// Check results
+		String result = "";
+		if (a + b == answer) {
+			result = "Correct!";
+			generateNumbers();
+		} else {
+			result = "Incorrect!";
+		}
+
+		Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
+		
+		// Clear input
+		user_input.setText("");
+	}
+
 
 }
